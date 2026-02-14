81d563b5c5ecfcd488870fdf41d3036ce4533a5f
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-02-13 21:59:00 -0400

    added task 8 which prints all numbers from base 16 in lowercase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char c = '0';
+while (c <= '9')
+{
+putchar(c);
+c++;
+}
+c = 'a';
+while (c <= 'f')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
