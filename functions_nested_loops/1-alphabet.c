fa0c66aa22eda12dff767f8398209f9093409e83
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-02-14 13:16:35 -0400

    added task 1 which prints the alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet - print alphabet
+*
+* Return: return alway 0
+*
+*/
+void print_alphabet(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+}
