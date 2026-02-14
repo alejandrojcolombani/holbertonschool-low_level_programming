7d49e7ab68c6fa3570a251904a2afc52c116d879
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-02-14 13:29:25 -0400

    added task 2 which prints the alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - prints alphabet 10 times
+*
+* Return: return alway 0
+*
+*/
+void print_alphabet_x10(void)
+{
+int i = 0;
+while (i < 10)
+{
+char c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+i++;
+_putchar('\n');
+}
+}
