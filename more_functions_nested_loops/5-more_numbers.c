9bcc2747f759f0c72b965aea184a5fdd804d722d
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-02-17 18:52:05 -0400

    added task 5 which prints 0-14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - prints the numbers from 0 to 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+int i;
+for (i = 0; i <= 10; i++)
+{
+int j;
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+{
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+}
+else
+{
+_putchar(j + '0');
+}
+}
+_putchar('\n');
+}
+}
