c2e76a473d2fe53f510c1f5930a266c8d0101fbc
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-02-19 10:54:33 -0400

    added task 9 which prints fizz buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - prints FizzBuzz from 1 to 100
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
+else
+{
+printf("%d", i);
+}
+if (i != 100)
+{
+printf(" ");
+}
+}
+return (0);
+}
