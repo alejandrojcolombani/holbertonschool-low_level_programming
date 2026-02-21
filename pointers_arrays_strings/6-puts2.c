380208c702334553fda99823981330358f66b350
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-02-21 10:30:20 -0400

    fixed task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,21 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character
- * @str: pointer to the string
+ * puts2 - Prints every other character of a string.
+ * @str: The string to be printed.
+ *
+ * Return: void.
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 
 	_putchar('\n');
