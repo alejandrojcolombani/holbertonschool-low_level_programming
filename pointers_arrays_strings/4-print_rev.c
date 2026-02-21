dc359e23b1bb02f8efc873e166e0ad75667f1fa6
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-02-20 22:40:32 -0400

    added task 4 which prints a string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to the string
+ */
+void print_rev(char *s)
+{
+int i;
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+while (s[i + 1] >= 0)
+{
+_putchar(s[i]);
+i--;
+}
+_putchar('\n');
+}
