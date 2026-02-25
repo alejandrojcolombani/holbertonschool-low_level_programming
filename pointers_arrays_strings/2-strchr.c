cded47b5af1c612f927ed44d3d6b634462d021a0
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-02-24 21:29:47 -0400

    added task 2 which locates a character in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search
+ * @c: character to find
+ *
+ * Return: pointer to the first occurrence of c,
+ * or NULL if c is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+
+	if (c == '\0')
+		return (s + i);
+
+	return (NULL);
+}
