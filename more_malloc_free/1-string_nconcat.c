26bb9b77bc300956757ca277a8ac99283b288cdd
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-03-04 13:44:18 -0400

    made the function under 40 lines

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -7,7 +7,7 @@
  * @s2: second string
  * @n: number of bytes from s2 to concatenate
  *
- * Return: pointer to the new string, or NULL if it fails
+ * Return: pointer to new string or NULL if it fails
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -16,45 +16,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
 	len1 = 0;
 	len2 = 0;
-
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
-
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
 
 	if (n >= len2)
-	{
 		n = len2;
-	}
 
 	ptr = malloc((len1 + n + 1) * sizeof(char));
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; i < len1; i++)
-	{
 		ptr[i] = s1[i];
-	}
-
 	for (j = 0; j < n; j++)
-	{
 		ptr[i + j] = s2[j];
-	}
 
 	ptr[i + j] = '\0';
-
 	return (ptr);
 }
