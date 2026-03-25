3a9d178fe54c833076df28452bfc817412bb3829
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-03-25 11:55:06 -0400

    added task 2 which adds a new node at the beggining of a list_t list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: address of pointer to head node
+ * @str: string to add
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	new->len = len;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
