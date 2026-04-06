d06cafa9a611bf355ebf1f2655bf9e55e67a9e0e
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-04-06 14:30:49 -0400

    added task 5 which prints a hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+	int first = 1;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+
+		while (temp)
+		{
+			if (!first)
+				printf(", ");
+
+			printf("'%s': '%s'", temp->key, temp->value);
+
+			first = 0;
+			temp = temp->next;
+		}
+	}
+
+	printf("}\n");
+}
