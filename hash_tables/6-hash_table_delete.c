247618d535a89d2b384c68a674930947bc2272a3
alejandrocolombani <alejandrojcolombani@gmail.com>
2026-04-06 14:39:49 -0400

    added task 6 which deletes a hash table

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+	hash_node_t *current;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			temp = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = temp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
